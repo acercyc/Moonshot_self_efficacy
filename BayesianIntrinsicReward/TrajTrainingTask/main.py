f4aabd598ccd31a363e426fd42eccd31bb11ed86
acercyc <acercyc@gmail.com>
2023-12-08 12:58:02 +0900

    Add PID

--- BayesianIntrinsicReward/TrajTrainingTask/main.py
+++ BayesianIntrinsicReward/TrajTrainingTask/main.py
@@ -5,6 +5,33 @@ from scipy.interpolate import interp1d
 import matplotlib.pyplot as plt
 
 
+# ---------------------------------------------------------------------------- #
+#                                  parameters                                  #
+# ---------------------------------------------------------------------------- #
+# target
+radius = 0.005
+vx_max = 0.05
+vy_max = 0.05
+
+# environment
+line_pos_up = 0.45
+line_pos_down = -0.40
+
+# assistance interface
+assistance_keyAdd = "right"
+assistance_keySub = "left"
+assistance_min = 0
+assistance_max = 10
+Kp, Ki, Kd = 0.1, 0.01, 0.001
+
+
+# Path
+path_n = 3
+path_frequency_range = (0.5, 1.3)
+nPathSamples = 1000
+
+
+
 def generate_path(n, frequency_range, start_y, end_y, nSample=1000, xy_ratio=0.25):
     """
     Generates a path by combining n sine waves with frequencies from the given range.
@@ -83,63 +110,80 @@ def fn_assistance(mouse_x, mouse_y, path_x, path_y, assistance):
     return mouse_x
 
 
-# %%
+class PIDController:
+    def __init__(self, Kp, Ki, Kd, K=1):
+        self.Kp = Kp
+        self.Ki = Ki
+        self.Kd = Kd
+        self.K = K
+        self.prev_error = 0
+        self.sum_error = 0
 
-# Parameters
-radius = 0.25
-line_pos_up = 0.45
-line_pos_down = -0.40
-vx_max = 0.05
-vy_max = 0.05
-assistance_keyAdd = "right"
-assistance_keySub = "left"
-assistance_min = 0
-assistance_max = 10
-isNewPath = False
+    def update(self, x, setpoint, dt, K=1):
+        self.K = K
+        error = setpoint - x
+        error_p = error * self.Kp
+        error_i = self.sum_error + error * self.Ki * dt
+        error_d = self.Kd * (error - self.prev_error) / dt
 
-# path parameters
-path_n = 3
-path_frequency_range = (0.5, 1.3)
+        control = error_p + error_i + error_d
 
-# start point
+        self.prev_error = error
+        self.sum_error = error_i
+
+        return control * self.K
+
+# ---------------------------------------------------------------------------- #
+#                                Initialization                                #
+# ---------------------------------------------------------------------------- #
+# start and end point
 start_x, start_y = 0, line_pos_down
 end_x, end_y = 0, line_pos_up
-
+isNewPath = False
+assistance = 0
 
 # create path
-nPathSamples = 1000
 path_x, path_y = generate_path(
     path_n, path_frequency_range, start_y, end_y, nSample=nPathSamples
 )
 plt.plot(path_x, path_y)
 plt.gca().set_aspect("equal", adjustable="box")
-# %%
 
-# Create a window
+
+
+# %%
+Kp, Ki, Kd = 1, 0.01, 0.001
+PID = PIDController(Kp, Ki, Kd)
+
+
+# ---------------------------------------------------------------------------- #
+#                               Psychopy stimuli                               #
+# ---------------------------------------------------------------------------- #
+# Create window
 win = visual.Window(
     [800, 800], units="height", checkTiming=False, color="black", fullscr=True
 )
 
-# boundary lines
-line_1 = visual.Line(win, start=(-0.5, start_y), end=(0.5, start_y), lineColor="white")
-line_2 = visual.Line(win, start=(-0.5, end_y), end=(0.5, end_y), lineColor="white")
+# up and down boundary lines
+line_down = visual.Line(win, start=(-0.5, start_y), end=(0.5, start_y), lineColor="white")
+line_up = visual.Line(win, start=(-0.5, end_y), end=(0.5, end_y), lineColor="white")
 
 
-# path stimulus
+# Path stimuli
+path_true = visual.ShapeStim(
+    win, vertices=list(zip(path_x, path_y)), closeShape=False, lineColor=[0.3, 0.3, 0.3]
+)
 
-curve = visual.ShapeStim(win, vertices=list(zip(path_x, path_y))
-, closeShape=False, lineColor=[0.3, 0.3, 0.3])
-curve.draw()
-
-curve_self = visual.ShapeStim(win, vertices=list(zip(path_x, path_y))
-, closeShape=False, lineColor=[-1, 1, -1])
+path_self = visual.ShapeStim(
+    win, vertices=list(zip(path_x, path_y)), closeShape=False, lineColor=[-1, 1, -1]
+)
 
 # target
 target = visual.Circle(
-    win, radius=0.005, fillColor="green", lineColor="None", pos=(start_x, start_y)
+    win, radius=radius, fillColor="green", lineColor="None", pos=(start_x, start_y)
 )
 
-# assistance rating
+# assistance rating interface
 assistance_rating = visual.RatingScale(
     win,
     low=assistance_min,
@@ -160,52 +204,59 @@ assistance_rating = visual.RatingScale(
 )
 
 
-# text for score
+# Score
 text_score = visual.TextStim(
     win, text="score: ", pos=(0, 0.4), color="white", height=0.05
 )
 
-# Draw the line
-line_1.draw()
-line_2.draw()
-text_score.draw()
-curve.draw()
-assistance_rating.draw()
-# Draw the start point
-target.draw()
-
-
-# Show it on the screen
-win.flip()
-
-# set assistance
-assistance = 0
-
-# record and display mouse position
+# mouse
 mouse = event.Mouse(win=win)
+
+
+# ---------------------------------------------------------------------------- #
+#                             Initialise experiment                            #
+# ---------------------------------------------------------------------------- #
+
+line_down.draw()
+line_up.draw()
+text_score.draw()
+path_true.draw()
+assistance_rating.draw()
+target.draw()
+win.flip()
 mouse.setVisible(0)
+
+
+# ---------------------------------------------------------------------------- #
+#                               Experiment Start                               #
+# ---------------------------------------------------------------------------- #
 for iTrial in range(100):
+    # --------------------------- Trail initialisation --------------------------- #
     # Reset mouse position
     xs = [start_x]
     ys = [start_y]
     target.fillColor = "red"
     target.pos = (start_x, start_y)
 
-    line_1.draw()
-    line_2.draw()
+    line_down.draw()
+    line_up.draw()
     target.draw()
-    curve.draw()
+    path_true.draw()
     win.flip()
     core.wait(1)
 
     target.fillColor = "green"
-    line_1.draw()
-    line_2.draw()
+    line_down.draw()
+    line_up.draw()
     target.draw()
     win.flip()
 
     mouse.setPos((start_x, start_y))
+    
+    # -------------------------------- Trial start ------------------------------- #
     while True:
+        
+        # ------------------- Detect and manipulate mouse position ------------------- #
         mouse_x, mouse_y = mouse.getPos()
 
         # if mouse_y is negative, change it to 0
@@ -226,7 +277,16 @@ for iTrial in range(100):
                 mouse_x = 0.5 * np.sign(mouse_x)
 
             # add assistance
-            mouse_x = fn_assistance(mouse_x, mouse_y, path_x, path_y, assistance)
+            # mouse_x = fn_assistance(mouse_x, mouse_y, path_x, path_y, assistance)
+            # PID
+            dt = mouse_y - ys[-1]
+            setpoint = np.interp(mouse_y, path_y, path_x)
+            mouse_x_asssistance = PID.update(mouse_x, setpoint, 1/60, assistance)
+            mouse_x += mouse_x_asssistance
+            
+            # # limit x velocity
+            # if np.abs(mouse_x - xs[-1]) > vx_max:
+            #     mouse_x = xs[-1] + vx_max * np.sign(mouse_x - xs[-1])
 
             # End trial if mouse reaches the end
             if mouse_y >= end_y:
@@ -236,16 +296,16 @@ for iTrial in range(100):
         target.pos = (mouse_x, mouse_y)
         mouse.setPos((mouse_x, mouse_y))
 
-        # Present stimuli
-        line_1.draw()
-        line_2.draw()
+        # ------------------------------ Present stimuli ----------------------------- #
+        line_down.draw()
+        line_up.draw()
         target.draw()
         assistance_rating.draw()
         win.flip()
 
         keys = event.getKeys()
 
-        # press 'q' to quit
+        # ------------------------------ Keybaord event ------------------------------ #
         if "q" in keys:
             win.close()
             core.quit()
@@ -264,6 +324,8 @@ for iTrial in range(100):
                 rating_ = assistance_min
             assistance_rating.setMarkerPos(rating_)
             isNewPath = True
+            
+        assistance = (assistance_rating.getRating() / assistance_max) ** 3
 
         xs.append(mouse_x)
         ys.append(mouse_y)
@@ -276,9 +338,9 @@ for iTrial in range(100):
     score = fn_score_rms(path_x, xs, start_y, end_y, nPathSamples)
     text_score.text = f"score: {score*100:.0f}"
     text_score.draw()
-    curve.draw()
-    curve_self.vertices = list(zip(xs, ys))
-    curve_self.draw()
+    path_true.draw()
+    path_self.vertices = list(zip(xs, ys))
+    path_self.draw()
     win.flip()
     core.wait(1)
 
@@ -288,9 +350,11 @@ for iTrial in range(100):
         )
         isNewPath = False
         assistance = (assistance_rating.getRating() / assistance_max) ** 3
-        curve.vertices = list(zip(path_x, path_y))
+        path_true.vertices = list(zip(path_x, path_y))
 
 # Close the window
 win.close()
 
+q# %%
 
+# %%
